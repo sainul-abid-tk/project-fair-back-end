a4960cacc00c3c405022e038deeb4e11b912c21c
sainul-abid-tk <sainulabidtk2468@gmail.com>
2024-01-29 13:05:58 +0530

    3rd

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,5 @@
 const users=require('../Models/userModel')
-
+const jwt=require('jsonwebtoken')
 exports.register=async(req,res)=>{
     const {username,email,password}=req.body
     try {
@@ -14,6 +14,24 @@ exports.register=async(req,res)=>{
             })
             await newUser.save()
             res.status(200).json(newUser)
+        }
+    }catch (err) {
+        res.status(401).json(err)
+    }
+}
+
+exports.login=async(req,res)=>{
+    const {email,password}=req.body
+    try {
+        // check email,password already exist
+        const existingUser = await users.findOne({ email,password })
+        console.log(existingUser);
+        if (existingUser) {
+            // generate token using jwt-
+            const token =jwt.sign({userId:existingUser._id},process.env.jwt_secret)
+            res.status(200).json({existingUser,token})
+        } else {
+            res.status(406).json("Invalid Email / Password")
         }
     }catch (err) {
         res.status(401).json(err)
--- Routes/routes.js
+++ Routes/routes.js
@@ -3,4 +3,7 @@ const router = express.Router()
 const userController=require('../Controllers/userController')
 // Route for register
 router.post('/register',userController.register)
+// Route for login
+router.post('/login',userController.login)
+
 module.exports = router
